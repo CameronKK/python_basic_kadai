9d535b80ce4501dd99919c39f5afa63e98a43230
Tomo <ktomotomo58sky@gmail.com>
2025-01-19 19:33:00 +0900

    Add files via upload

---
+++ kadai_011/kadai_011.py
@@ -0,0 +1,12 @@
+array = ["水", "金", "地", "火", "木", "土", "天", "海", "冥"]
+
+array_count = len(array)
+
+for i in array:
+    print(i)
+    
+index = 0
+
+while index < array_count:
+    print(array[index])
+    index += 1
